1d03b52c9101143cb9426647c69a177354041ec6
Oliver Wise <owise1@gmail.com>
2016-01-07 16:08:55 -0800

    catching more ads

--- js/document_end.js
+++ js/document_end.js
@@ -2,8 +2,7 @@ jQuery(function ($){
   var howMany = 3
   var tried = 0
   ;(function checkIFrames() {
-    console.log('here')
-    $('iframe[id^=google_ads_iframe]').each(function (){
+    $('iframe[id^=google_ads],ins.adsbygoogle,ins[id^=aswift]').each(function (){
       artAdder.processAdNode(this)
     })
     if (++tried < howMany) {
